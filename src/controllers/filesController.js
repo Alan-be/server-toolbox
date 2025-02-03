4f480665da4dcc99ff3fc025a1c5be261ebc2b9d
Alan-be <alan.av07@hotmail.com>
2025-02-03 17:21:57 -0600

    feat: get one file (query param)

--- src/controllers/filesController.js
+++ src/controllers/filesController.js
@@ -2,33 +2,36 @@ const apiServies = require('../services/apis.js')
 const { parseFile } = require('../utils/utils.js')
 
 exports.getFilesData = async (req, res) => {
-  try {
-    const results = []
-    const filesList = await apiServies.getFiles()
-
-    for (const fileName of filesList) {
-      const fileContent = await apiServies.downloadFiles(fileName)
-
-      if (fileContent) {
-        const parseData = parseFile(fileContent, fileName)
-        if (parseData) {
-          results.push(parseData)
+    try {
+      const { fileName } = req.query;
+      const results = [];
+      const filesList = await apiServies.getFiles();
+  
+      const filteredFilesList = fileName ? filesList.filter(file => file === fileName) : filesList;
+  
+      for (const fileName of filteredFilesList) {
+        const fileContent = await apiServies.downloadFiles(fileName);
+  
+        if (fileContent) {
+          const parseData = parseFile(fileContent, fileName);
+          if (parseData) {
+            results.push(parseData);
+          }
         }
       }
+  
+      res.status(200).json({
+        status: 'success',
+        data: results
+      });
+    } catch (error) {
+      console.log('An error ocurred while fetching files: ', error.message);
+      res.status(500).json({
+        status: 'error',
+        message: 'Failed to get file list'
+      });
     }
-
-    res.status(200).json({
-      status: 'success',
-      data: results
-    })
-  } catch (error) {
-    console.log('An error ocurred while fetching files: ', error.message)
-    res.status(500).json({
-      status: 'error',
-      message: 'Failed to get file list'
-    })
-  }
-}
+  };
 
 
 exports.getListFiles = async(req,res) => {
